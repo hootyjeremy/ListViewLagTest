3e687ceb087695d0a4b879ac2407fcf399646b40
hootyjeremy <68588318+hootyjeremy@users.noreply.github.com>
2023-05-30 13:58:22 -0500

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ListViewTest.ViewModel;
+using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+
+namespace ListViewTest;
+
+public partial class MainPage : ContentPage
+{
+    //int count = 0;
+
+    //List<string> lstTest = new();
+    ObservableCollection<string> ocList = new();
+
+    public MainPage()
+	{
+		InitializeComponent();
+
+        //lstTest.Add("string1");
+
+        lstListView.ItemsSource = ocList;
+
+        //ListViewModel lvm = new();
+
+        //lvm.Names = new(); // $ declare when and where?
+        //lvm.Names.Add("buddy garrity");
+        //lvm.Names.Add("jason priestley");
+        //lvm.Names.Add("luke perry");
+
+        //lstListView2.ItemsSource = lvm.Names;
+    }
+
+    private void btnAddLine_Clicked(object sender, EventArgs e)
+    {
+        ocList.Add("new line");
+    }
+
+    private void btnClear_Clicked(object sender, EventArgs e)
+    {
+        ocList.Clear();
+    }
+}
+
---
+++ ViewModel/ListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewTest.ViewModel
+{
+    partial class ListViewModel : ObservableObject
+    {
+        // $ read this to proceed with mvvm ListView...
+        // https://learn.microsoft.com/en-us/dotnet/communitytoolkit/mvvm/observableobject
+        // https://learn.microsoft.com/en-us/dotnet/communitytoolkit/mvvm/generators/observableproperty
+
+        [ObservableProperty]
+        List<string> names;
+
+        
+
+    }
+}
